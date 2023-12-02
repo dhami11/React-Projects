b43ab52d0ca4714a9b05300ee4e77b72aadb765a
Ahmad Dhami <dhami451@gmail.com>
2023-12-03 00:08:04 +0500

    redux implemented

---
+++ 10reduxToolkitToDo/src/App.jsx
@@ -0,0 +1,20 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+import AddToDo from './components/AddToDo'
+import Todos from './components/Todos'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+    <h1>redux</h1>
+    <AddToDo/>
+    <Todos/>
+    </>
+  )
+}
+
+export default App
